37d68b198a65855f49cf4694de9b9e1d0308d27e
Ridhanovita <novitaridha796@gmail.com>
2025-05-07 19:49:46 +0700

    proses binary search

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -81,6 +81,22 @@ void binarySearch()
         int low = 0;
         int high = nPanjang - 1;
 
-        
+        do
+        {
+            int mid = (low = high) / 2;
+            if (element[mid] == x)
+            {
+                cout << "\n[v] Elemen " << x << " ditemukan pada indexs " << mid << ".\n";
+                return;
+            }
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
+        } while (low <= high);
     } 
 }
