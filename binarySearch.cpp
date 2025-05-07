eb6e7aaa328a932f5586b56d5da84f2d0bf7d201
Ridhanovita <novitaridha796@gmail.com>
2025-05-07 19:25:51 +0700

    input data

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -4,3 +4,31 @@ using namespace std;
 int element[101];
 int nPanjang;
 int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nJumlah elemen tidak boleh lebih dari 10. Silakan coba lagi.\n";
+        }
+    }
+
+    cout << "==============================" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "==============================" << endl;
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> element[i];
+    }
+}
